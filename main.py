4706f5ff935437442825cf4dec9c60c8fa5050f9
𝐷𝑒𝑙𝑡𝟺_𝐻𝑎𝑧𝑒 <darknetfert@gmail.com>
2026-04-17 00:14:15 -0600

    Implement interactive command loop in main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+while True:
+    cmd = input(">> ")
+
+    if cmd == "hola":
+        print("Hola 👋")
+
+    elif cmd == "run":
+        print("Ejecutando...")
+
+    elif cmd == "exit":
+        print("Bye 👋")
+        break
+
+    else:
+        print("Comando no reconocido")
