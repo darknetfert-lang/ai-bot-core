a66e613ae72fce75000eedf2ea379d27b2cf6eb1
𝐷𝑒𝑙𝑡𝟺_𝐻𝑎𝑧𝑒 <darknetfert@gmail.com>
2026-04-17 00:16:42 -0600

    Add help menu and improve command feedback

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import os
 
+def help_menu():
+    print("""
+Comandos disponibles:
+- hola
+- run
+- open <app>
+- clear
+- exit
+- help
+""")
+
 while True:
-    cmd = input(">> ")
+    cmd = input(">> ").lower()
 
     if cmd == "hola":
         print("Hola 👋")
@@ -11,14 +22,18 @@ while True:
 
     elif cmd.startswith("open "):
         app = cmd.replace("open ", "")
+        print(f"Abriendo {app}...")
         os.system(app)
 
     elif cmd == "clear":
         os.system("clear")
+
+    elif cmd == "help":
+        help_menu()
 
     elif cmd == "exit":
         print("Bye 👋")
         break
 
     else:
-        print("Comando no reconocido")
+        print("Comando no reconocido (usa 'help')")
